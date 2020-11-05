4436a5ae278bf34cdcb5024d2390524a0055ad31
jcfl1 <jcfl@cin.ufpe.br>
2020-11-05 15:38:42 -0300

    Created the sendSessionResult method in server, receiveSessionResult in client, a test of these 2 methods and a small modification in VotingSession class

--- client.py
+++ client.py
@@ -2,8 +2,11 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes, hmac
 from cryptography import exceptions
+from exceptions import InvalidPacket
+from VotingSession import VotingSession
 import os
 import cripto
+import json
 
 
 class VotingClient:
@@ -54,4 +57,76 @@ class VotingClient:
         message = b"".join([message, tag])
         encryptedMacKey = cripto.encryptWithPublicKey(self.serverPublicKey, macKey)
         message = b"".join([message, encryptedMacKey])
-        return message
+        return message, nonce, macKey
+
+    
+    """
+        Recieve session result packet
+
+        Args:
+            The packet sent from the server method "server.sendSessionResult()"
+            The nonce used in "client.verifySession()"
+            The HMACKey used in "client.verifySession()"
+        Returns:
+            Nothing, so it raises an exception (means that either the nonce, or the tag is invalid),
+            Or a string signaling an error, 
+            Or a session object
+    """
+    def recieveSessionResult(self, packet, lastNonce, HMACKey):
+        
+        errorSz = 5
+        nonceSz = 48
+        tagSz = 32
+        invalidTagSz = len("Invalid tag")
+
+        try:
+            anErrorOccur = packet[:errorSz].decode() == "ERROR"
+        except:
+            anErrorOccur = False
+
+
+        if anErrorOccur:
+
+            nonce = packet[errorSz:(errorSz + nonceSz)]
+            
+            if nonce != lastNonce:
+                raise InvalidPacket
+
+            else:
+                if cripto.verifyTag(HMACKey, packet[:-tagSz], packet[-tagSz:]):
+                    if packet[(errorSz + nonceSz):(errorSz + nonceSz + invalidTagSz)] == "Invalid Tag":
+                        return "The packet that you sent had an invalid tag"
+                    else:
+                        return "This session is still not finished"
+                else:
+                    raise InvalidPacket
+
+        else:
+
+            nonce = packet[:nonceSz]
+            byteDumpedSession = packet[nonceSz:-tagSz]
+            tag = packet[-tagSz:]
+
+            if nonce != lastNonce:
+                raise InvalidPacket
+            
+            else:
+                if cripto.verifyTag(HMACKey, b"".join([nonce, byteDumpedSession]), tag):
+                    
+                    sessionDict = json.loads(byteDumpedSession.decode())
+
+                    # Now, we have to convert the sessionDict to an object session
+
+                    requestedSession = VotingSession(
+                        sessionName= sessionDict["id"],
+                        candidates= sessionDict["candidates"],
+                        sessionMode= sessionDict["sessionMode"],
+                        duration= sessionDict["duration"],
+                        maxVotes= sessionDict["maxVotes"],
+                        candidatesFormat= "Dictionary"
+                    )
+
+                    return requestedSession
+
+                else:
+                    raise InvalidPacket
---
+++ test_session_result.py
@@ -0,0 +1,36 @@
+from client import VotingClient
+from server import VotingServer
+import VotingSession
+
+candidatos = ["julio", "divanilson", "erick jacquin", "caio", "xuxa"]
+
+nonceSz = 48
+tagSz = 32
+headerSz = 2
+
+privK = open('./tests/keys/client_test_keys.pem', 'rb').read()
+pubK = open('./tests/keys/client_test_keys.pub', 'rb').read()
+svPubK = open('./tests/keys/server_test_keys.pub', 'rb').read()
+svPrivK = open('./tests/keys/server_test_keys.pem', 'rb').read()
+
+testServer = VotingServer(svPrivK, svPubK)
+testServer.sessions["concurso melhor pizza da minha rua"] = VotingSession.VotingSession("concurso melhor pizza da minha rua", candidatos, "maxVotes", maxVotes=19)
+testClient = VotingClient(privK, pubK, svPubK)
+
+# Teste quando a quantidade de votos desejada ainda não foi atingida
+print("Primeiro teste")
+fstPacket, nonce, HMACKey = testClient.verifySession("concurso melhor pizza da minha rua")
+sndPacket = testServer.sendSessionResult(fstPacket)
+print(testClient.recieveSessionResult(sndPacket, nonce, HMACKey))
+
+# Teste quanda a quantidade de votos maxima foi atingida
+print("")
+print("Segundo teste")
+testServer.sessions["concurso melhor pizza da minha rua"].candidates["julio"] = 10
+testServer.sessions["concurso melhor pizza da minha rua"].candidates["erick jacquin"] = 9
+fstPacket, nonce, HMACKey = testClient.verifySession("concurso melhor pizza da minha rua")
+sndPacket = testServer.sendSessionResult(fstPacket)
+requestedSession = testClient.recieveSessionResult(sndPacket, nonce, HMACKey)
+
+
+print(requestedSession.candidates)
