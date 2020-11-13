0d31c0856b8abd6e67b3026c8af5ae280dc4d280
iraline <iraline@hotmail.com>
2020-11-13 03:35:12 -0300

    Ajust to the import works

--- tests/test_biblioteca_server.py
+++ tests/test_biblioteca_server.py
@@ -1,11 +1,14 @@
+import sys
+sys.path.append("..") # Adds higher directory to python modules path.
+
 from biblioteca import Biblioteca
 from VotingSession import VotingSession
 
 usersInfoStorage = "../usersInfoStorage.json"
 usersCredentials = "../usersPubKeys.json"
 
-svPubK = open('.keys/server_test_keys.pub', 'rb').read()
-svPrivK = open('.keys/server_test_keys.pem', 'rb').read()
+svPubK = open('keys/server_test_keys.pub', 'rb').read()
+svPrivK = open('keys/server_test_keys.pem', 'rb').read()
 
 Server = Biblioteca("localhost", 1234, svPubK, usersInfoStorage, usersCredentials, svPrivK)
 
